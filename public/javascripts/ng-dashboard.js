18d7a0d1793f1ce47c00e629fdc6a9a81be360bd
S G <sachan.ganesh@gmail.com>
2014-09-18 15:49:22 -0700

    Link angular to backend

---
+++ public/javascripts/ng-dashboard.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('DashboardApp', []);
+
+app.controller('DashCntrl', function ($scope) {
+    $scope.user = {
+        name: null,
+        game: null
+    }
+    $scope.initUser = function (userName, gameData) {
+        $scope.user.name = userName;
+        $scope.user.game = gameData;
+        console.log($scope.user.name.toString());
+    }
+});
