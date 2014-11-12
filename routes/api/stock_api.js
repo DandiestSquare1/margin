3276669b43e9ef53065d5261a7344f0ca8ec3a43
S G <sachan.ganesh@gmail.com>
2014-11-11 20:45:11 -0800

    Stock and transaction model creation routes

--- routes/api/stock_api.js
+++ routes/api/stock_api.js
@@ -1,47 +1,78 @@
 ﻿var Q = require('q');
 var request = require('request');
+var Stock = require('../../models/stock');
+var Transaction = require('../../models/transaction');
 
 exports.lookupData = function (req, res) {
-    function callAPI() {
-        var deferred = Q.defer();
-        if (req.params.ticker)
-            request('http://dev.markitondemand.com/Api/v2/Lookup/json?input=' + req.params.ticker, function (err, resp, body) {
-                if (err)
-                    deferred.reject(err);
-                if (resp) {
-                    if (resp.statusCode == 200)
-                        deferred.resolve(body);
-                } else
-                    deferred.reject({ error: 'Empty params' });
-
-            });
-        else
-            deferred.reject({ error: 'Empty params' });
-        return deferred.promise;
-    }
-    callAPI().then(function (data) {
+    callAPI(req, "Lookup").then(function (data) {
         res.json(JSON.parse(data));
     });
 };
 
 exports.quoteData = function (req, res) {
-    function callAPI() {
-        var deferred = Q.defer();
-        if (req.params.ticker)
-            request('http://dev.markitondemand.com/Api/v2/Quote/json?symbol=' + req.params.ticker, function (err, resp, body) {
-                if (err)
-                    deferred.reject(err);
-                if (resp) {
-                    if (resp.statusCode == 200)
-                        deferred.resolve(body);
-                } else
-                    deferred.reject({ error: 'Empty params' });
-            });
-        else
-            deferred.reject({ error: 'Empty params' });
-        return deferred.promise;
-    }
-    callAPI().then(function (data) {
+    callAPI(req, "Quote").then(function (data) {
         res.json(JSON.parse(data));
     });
 };
+
+exports.isBought = function (req, res) {
+    var newTransaction, newStock;
+    if (req.body.numberOfShares && req.body.numberOfShares > 0) {
+        newTransaction = new Transaction();
+        newStock = new Stock();
+        newStock.symbol = req.params.ticker;
+        newStock.amount = req.body.numberOfShares;
+        newStock.valueChange = 0;
+        callAPI(req, "Quote").then(function (data) {
+            newStock.price = data.LastPrice;
+            newTransaction.time = Date.now();
+            newTransaction.grossCost = newStock.amount * newStock.price;
+            newStock.user = req.user._id;
+            newTransaction.stock = newStock._id;
+            newTransaction.user = req.user._id;
+            if (newTransaction.grossCost <= req.user.game.amount) {
+                newStock.save(function (err) {
+                    if (err)
+                        console.log(err);
+                });
+                newTransaction.save(function (err) {
+                    if (err)
+                        console.log(err);
+                });
+                req.user.game.stocks.push(newStock);
+                req.user.game.transactions.push(newTransaction);
+                req.user.game.amount = req.user.game.amount - newTransaction.grossCost;
+                req.user.save(function (err) {
+                    if (err)
+                        console.log(err);
+                });
+            }
+        });
+        req.flash('notice', 'Processed transaction.');
+    } else {
+        req.flash('notice', 'Could not process transaction. Try again.');
+    }
+    res.redirect('/stock/' + req.params.ticker);
+};
+
+
+function callAPI(req, data) {
+    var deferred = Q.defer();
+    if (req.params.ticker)
+        request('http://dev.markitondemand.com/Api/v2/' + data + '/json?symbol=' + req.params.ticker, function (err, resp, body) {
+            if (err)
+                deferred.reject(err);
+            if (resp) {
+                if (resp.statusCode == 200)
+                    deferred.resolve(body);
+            } else
+                deferred.reject({ error: 'Empty params' });
+        });
+    else
+        deferred.reject({ error: 'Empty params' });
+    return deferred.promise;
+}
+
+function settleDues() {
+    var deferred = Q.defer();
+}
--- routes/api/transaction_api.js
+++ routes/api/transaction_api.js
@@ -2,37 +2,10 @@
 var Transaction = require('../../models/transaction');
 
 exports.getData = function (req, res) {
-    res.json({ success : true });
-};
-
-exports.create = function (req, res) {
-    function createRequest() {
-        var deferred = Q.defer();
-        if (req.body.symbol && req.body.amount && req.body.price) {
-            var newRequest = new Transaction();
-            newRequest.symbol = req.body.symbol;
-            newRequest.time = Date.now();
-            newRequest.amount = req.body.amount;
-            newRequest.price = req.body.price;
-            newRequest.user = req.user._id;
-            newRequest.save(function (err) {
-                if (err)
-                    deferred.reject(err);
-                else {
-                    console.log('Transaction successfully created.');
-                    deferred.resolve({
-                        transaction: newRequest,
-                        amount     : req.user.game.amount - (req.body.price * req.body.amount)
-                    });
-                }
-            });
-        } else
-            deferred.reject({ error: 'Empty params' });
-        return deferred.promise;
-    }
-    createRequest().then(function (data) {
-        res.json(data);
-    }, function (err) {
-        console.log(err);
+    Transaction.find({}, function(err, transactions) {
+        res.send(transactions.reduce(function(transactionMap, item) {
+            transactionMap[item.id] = item;
+            return transactionMap;
+        }, {}));
     });
 };
